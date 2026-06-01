e8a123bd17e6e7caa00dddcb86b502e296568c47
Brian Wance <brian.wance@gaf.com>
2026-06-01 13:43:53 -0700

    Remove test scripts

--- gymlog-react/test_local.cjs
+++
@@ -1,6 +0,0 @@
-
-const fs = require("fs");
-let content = fs.readFileSync("src/components/CircuitView.jsx", "utf8");
-content = content.replace("const handleLogSet = async (ex, logs) => {", "const handleLogSet = async (ex, logs) => {\n        console.log(\"handleLogSet CALLED\", {ex, logs});");
-content = content.replace("if (entries.length > 0) {", "console.log(\"ENTRIES:\", entries);\n        if (entries.length > 0) {");
-fs.writeFileSync("src/components/CircuitView.jsx", content);
